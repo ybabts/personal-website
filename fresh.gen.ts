e1ae287e9b4146dad82e917d838d28b8f347660b
Tyler Mueller <muelltyl@gmail.com>
2023-06-11 02:00:51 -0500

    removes unnessecary parts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,24 +3,17 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/index.tsx";
-import * as $4 from "./routes/pwa/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $0 from "./routes/_app.tsx";
+import * as $1 from "./routes/_middleware.ts";
+import * as $2 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/_app.tsx": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/index.tsx": $3,
-    "./routes/pwa/index.tsx": $4,
+    "./routes/_app.tsx": $0,
+    "./routes/_middleware.ts": $1,
+    "./routes/index.tsx": $2,
   },
-  islands: {
-    "./islands/Counter.tsx": $$0,
-  },
+  islands: {},
   baseUrl: import.meta.url,
   config,
 };
