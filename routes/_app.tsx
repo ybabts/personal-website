bbbe2d4cc92498d71e8042ed5559596768e6e0d3
Tyler Mueller <muelltyl@gmail.com>
2023-06-11 01:57:27 -0500

    adds the web manifest to the app head

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,6 +5,7 @@ export default function App({ Component }: AppProps) {
   return (
     <>
       <Head>
+        <link rel="manifest" href="/manifest.webmanifest" />
       </Head>
       <title>Default Title</title>
       <Component />
